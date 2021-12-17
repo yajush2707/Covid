28cae967e3d723a4ca7862bded77543f70deea4a
yajush2707 <95477320+yajush2707@users.noreply.github.com>
2021-12-17 20:19:18 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from api_co import dataProcessor
+from tkinter import *
 
+global covid_cases
+global b
+covid_cases = "1"
+root = Tk()
+root.title('COVID19 REPRESENTATION')
+root.geometry("500x500")
 
 app = Flask(__name__)
 api = Api(app)
+
 
 class Global_data(Resource):
     '''
@@ -12,7 +20,7 @@ class Global_data(Resource):
     It has a get funtion that return a list of json
     of corona virus status scraped from
     https://www.worldometers.info/coronavirus/
-    
+
     The first item in list is a dictionary of world
     covid-19 status.
     The second item in list is a list of dictionary
@@ -20,33 +28,15 @@ class Global_data(Resource):
     The third item in the list are dictionary's of
     covid-19 status by continent.
     '''
-   
+
     def get(self):
-    
-       self.datum=dataProcessor().globalwise()
-       return self.datum
 
-class Country_data(Resource):
-    '''
-    It is the class of second endpoint '/country=<string:country>'
-    It has a get funtion that return a json
-    of corona virus status scraped from
-    https://www.worldometers.info/coronavirus/
-    
-    country is the parameter passed to the endpoint.
-    ''' 
+      self.datum = dataProcessor().globalwise()
+      global a
+      a = self.datum
+      #covid_cases = a[0][clicked.get()]
+      return self.datum
 
-    def get(self,country):
-        self.country=country
-        self.datum=dataProcessor().countrywise()
-        try:
-            for i in self.datum:
-                if i['Country/other']== self.country:
-                    self.countr_y=i
-                    break
-            return self.countr_y
-        except:
-            return "Make sure to enter proper input and its method"
 
 class Continent_data(Resource):
     '''
@@ -73,6 +63,8 @@ class Continent_data(Resource):
     def get(self,continent):
         self.continent=continent
         self.datum=dataProcessor().continentwise()
+        b= self.datum
+        
         try:
             for i in self.datum:
                 if i['Country/other']== self.continent:
@@ -82,10 +74,82 @@ class Continent_data(Resource):
         except:
             return "Make sure to enter proper input and its method"
 
+      
 #endpoints of the api
 api.add_resource(Global_data, '/')
-api.add_resource(Country_data,'/country=<string:country>')
 api.add_resource(Continent_data,'/continent=<string:continent>')
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+Global_data().get()
+#covid_cases = a[0][clicked.get()]
+#print (covid_cases)
+ 
+
+options = [
+  "- Select -",
+  "CoronavirusCases",
+  "NewCases",
+  "TotalDeaths",
+  "NewDeaths",
+  "TotalRecovered",
+  "NewRecovered",
+  "ActiveCases",
+  "Serious",
+  "Totalcases/1Mpop",
+   "Deaths/1Mpop",
+]
+clicked = StringVar()
+clicked.set(options[1])
+
+options1 = [
+  "Africa",
+    "Asia",
+    "Europe",
+    "Oceania",
+    "North America",
+    "South America",
+    ]
+
+
+
+clicked1= StringVar()
+#clicked1.set(options1[0])
+
+
+drop = OptionMenu(root, clicked, *options)
+drop2 = OptionMenu(root, clicked1, *options1)
+def submit():
+    #Clear the text boxe
+    covid_cases = a[0][clicked.get()]
+    parameter_show = Label(root, text= covid_cases)
+    parameter_show.grid(row= 8, column=2)
+    
+    
+    clicked.set(options[0])
+    clicked1.set(options1[0])
+    
+    
+    
+
+f_name = drop2
+f_name.grid(row=0, column= 1, padx = 20)
+l_name = drop 
+l_name.grid(row=5, column= 1, padx = 20)
+
+f_name_label = Label(root, text = "Country Name- ")
+f_name_label.grid(row= 0, column=0)
+l_name_label = Label(root, text = "Parameter Choice-  ")
+l_name_label.grid(row= 5, column=0)
+
+submit_btn = Button(root, text= "SUBMIT", command = submit)
+submit_btn.grid(row =6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+
+
+
+
+root.mainloop()
+if __name__ == '__main__':
+   app.run(debug=True)
+
+
+
+
